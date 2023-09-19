1a664eeafb7b984489076aef5090260aaa5580a7
Dileep Gadiraju <89640648+dileep-gadiraju@users.noreply.github.com>
2023-09-19 10:47:42 +0530

    Kafka Health Checks

--- src/main/java/pl/piomin/services/custom/health/KafkaHealthIndicator.java
+++ src/main/java/pl/piomin/services/custom/health/KafkaHealthIndicator.java
@@ -1,8 +1,11 @@
 package pl.piomin.services.custom.health;
 
 import io.micronaut.context.annotation.Property;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.health.HealthStatus;
+import io.micronaut.management.endpoint.health.HealthEndpoint;
 import io.micronaut.management.health.indicator.HealthIndicator;
 import io.micronaut.management.health.indicator.HealthResult;
 import jakarta.inject.Inject;
@@ -11,6 +14,9 @@ import pl.piomin.services.config.KafkaClientConfig;
 
 import java.util.Collections;
 
+@Replaces(KafkaHealthIndicator.class)
+@Requires(property = HealthEndpoint.PREFIX + ".url.enabled", value = "true")
+@Requires(beans = HealthEndpoint.class)
 public class KafkaHealthIndicator implements HealthIndicator {
 
     @Inject
@@ -31,7 +37,7 @@ public class KafkaHealthIndicator implements HealthIndicator {
             message=e.getMessage();
             healthStatus=HealthStatus.DOWN;
         }
-        healthResult= HealthResult.builder("custom-kafka", healthStatus)
+        healthResult= HealthResult.builder("kafka", healthStatus)
                 .details(Collections.singletonMap("details", message))
                 .build();
         return Publishers.just(healthResult);
